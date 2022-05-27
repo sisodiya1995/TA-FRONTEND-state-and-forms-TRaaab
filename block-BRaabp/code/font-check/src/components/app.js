78fb0ebec33e260a3bdaafb8e7771352f7265dcf
Akash Singh <akashingh95@gmail.com>
2022-05-27 16:15:20 +0530

    font-checking

---
+++ block-BRaabp/code/font-check/src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import From from './form';
+function App(){
+    return(
+        <>
+        <h2>Font Checking</h2>
+        <From />
+        </>
+    )
+}
+
+export default App;
