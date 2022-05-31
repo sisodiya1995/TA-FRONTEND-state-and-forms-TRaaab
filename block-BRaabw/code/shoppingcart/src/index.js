0a35aad4afd4225c0b029215b332da14ad5bf89a
Akash Singh <akashingh95@gmail.com>
2022-05-31 16:28:06 +0530

    shoppingcart

--- block-BRaabw/code/shoppingcart/src/index.js
+++ block-BRaabw/code/shoppingcart/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-export default function App() {
-  return <div>Hello World!</div>;
-}
+import App from './components/app'
 
 render(<App />, document.getElementById("root"));
